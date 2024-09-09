26a8661aba54e3fa7aac426d96233c858c637be2
Ayomikun Oyeniyi <gideonoye1@gmail.com>
2024-09-08 21:22:23 -0400

    Complete first phase of implementation

--- cipher.rb
+++ cipher.rb
@@ -1,14 +1,20 @@
 def caesar_cipher(string, shift)
   string_array = string.split("")
   
-  ciphered_num_array = string_array.map {|letter| letter.ord + shift}
+  ciphered_num_array = string_array.map {|letter| letter.downcase.ord + shift}
 
   ciphered_num_array.map! do |number|
     number > 122 ? 97 + (number - 122) : number
   end
 
-  ciphered_num_array
+  ciphered_string_array = ciphered_num_array.map {|number| number.chr}
+
+  ciphered_string_array.map! do |letter|
+    letter == "%" ? " " : letter
+  end
+
+  cipher = ciphered_string_array.join("").capitalize
 
 end
 
-p caesar_cipher("Whav a day", 5)
+p caesar_cipher("Whav a day?", 5)
